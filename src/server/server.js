838e2e46b1d43a4d48e28ab518b22c157ecbc188
DrVonDevious <drvondevious@gmail.com>
2020-05-03 21:58:27 -0500

    Fix server port address

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server, {});
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var SOCKETS = {};
 
